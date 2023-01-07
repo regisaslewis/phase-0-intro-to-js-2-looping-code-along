b1485dc10e3545c641e49645ed6b59d482bb926c
RegisLewis <regisaslewis@gmail.com>
2023-01-06 18:03:25 -0800

    commit tree 8904d9bb9b35377f1e262409c45dc628a47b0cde

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, event){
+    let message = [];
+    for (let i = 0; i < array.length; i++) {
+        message.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    };
+    return message;
+};
+
+function countDown() {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    };
+}
