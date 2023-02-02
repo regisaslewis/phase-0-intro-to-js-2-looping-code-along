6b7967cdd05bcc6bc87611a9bc0edb222511708b
RegisLewis <regisaslewis@gmail.com>
2023-02-01 18:55:27 -0800

    loop code-a-long

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return newArray;
+};
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+};
